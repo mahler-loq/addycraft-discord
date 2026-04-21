bc7ee8c6441c3d1c04daa81b85ef27e9c2c2463b
mahler-loq <276624538+mahler-loq@users.noreply.github.com>
2026-04-21 21:00:13 +0200

    Create constfile and make main.py runnable

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 from discord.ext import commands
-import os, dotenv, discord, logging
+import os, dotenv, discord, logging, glob, traceback
+import src.cnst as cnst
 dotenv.load_dotenv()
 logging.basicConfig(level=logging.INFO)
+rootlogger = logging.getLogger()
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=("-"),intents=discord.Intents.all()) # .all() will be changed, this is for testing
+    async def setup_hook(self):
+        for cogfile in glob.glob("./src/cogs/*.py"):
+            # Attempts to load each functionality in the bot
+            try:
+                await self.load_extension("src.cogs."+cogfile.removesuffix(".py"))
+            except Exception:
+                rootlogger.error(f"Error loading cog {cogfile}:")
+                rootlogger.error("".join(traceback.format_exception()))
+    async def on_ready(self):
+        rootlogger.info("Logged in successfully as {}!".format(self.user))
+        rootlogger.info("Hello from on_ready coroutine, preparing everything...")
+        rootlogger.info("syncing command tree with discord...")
+        try:await self.tree.sync()
+        except Exception:rootlogger.error(traceback.format_exc())
+        rootlogger.info("tree synced successfully!")
+        rootlogger.info("on_ready done!")
+if __name__ == "__main__":
+    bot = Bot()
+    try:
+        rootlogger.info("--- START ---")
+        if not os.getenv("TOKEN"):
+            rootlogger.error("no TOKEN found in environment! please add a .env file with the token, or set the environment variable in another way.")
+            raise Exception("no TOKEN found in environment!")
+        bot.run(os.getenv("TOKEN"))
+    except Exception:
+        rootlogger.error("Error starting bot:")
+        rootlogger.error("".join(traceback.format_exception()))
---
+++ src/cnst.py
@@ -0,0 +1,13 @@
+# This file contains hard-coded variables that are used across the bot,
+# This is to avoid hardcoding things in multiple places.
+
+developers_uid = {
+    "mahler": 1383041519371685958,
+    "adzel": 142854071647338496
+}
+developers = list(developers_uid.values())
+
+main_server_id = 1451022861706989712
+staff_server_id = 1451282482103062653
+
+#TODO: add role IDs, helper functions for determining ranks, etc.
